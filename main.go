f8648bd99b8935826a6d6f973b5e553fe781798c
Guilherme Frankenberger <guirerume@proton.me>
2025-02-04 22:18:54 -0300

    refactor: clear project

--- main.go
+++ main.go
@@ -1,33 +1,4 @@
 package main
 
-import (
-	"crypto-scrope/app"
-	"log"
-
-	"github.com/hajimehoshi/ebiten/v2"
-)
-
 func main() {
-	if err := run(); err != nil {
-		log.Fatalf("Failed to run the application: %v", err)
-	}
-}
-
-func run() error {
-	if err := configureWindow(); err != nil {
-		return err
-	}
-
-	application := app.New()
-
-	return ebiten.RunGame(application)
-}
-
-func configureWindow() error {
-	w, h := ebiten.Monitor().Size()
-	ebiten.SetWindowSize(w, h)
-	ebiten.SetWindowTitle("Crypto Scope v0.1")
-	ebiten.SetWindowPosition(0, 0)
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	return nil
 }
