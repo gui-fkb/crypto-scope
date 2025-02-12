16354fd7b8b0a9e941c32c759211474b870dbfe0
Guilherme Frankenberger <guirerume@proton.me>
2025-02-11 23:19:41 -0300

    feat: update trade widget render trades and flash on price change

--- app/trade_widget.go
+++ app/trade_widget.go
@@ -32,7 +32,7 @@ type tradeRowWidget struct {
 
 	price    *widget.Text
 	quantity *widget.Text
-	date     *widget.Text
+	time     *widget.Text
 }
 
 func NewTradeWidget() *tradeWidget {
@@ -64,9 +64,13 @@ func NewTradeWidget() *tradeWidget {
 		container.AddChild(tRow)
 	}
 
+	flash := ebiten.NewImage(1, 1)
+	flash.Fill(color.RGBA{255, 255, 255, 128})
+
 	return &tradeWidget{
 		Container: container,
 		rows:      rows,
+		flash:     flash,
 	}
 }
 
@@ -144,6 +148,42 @@ func NewTradeRowWidget() *tradeRowWidget {
 
 		price:    textA,
 		quantity: textB,
-		date:     textC,
+		time:     textC,
 	}
+}
+
+var lastPrice float64
+
+func (w *tradeWidget) Render(screen *ebiten.Image) {
+	if len(MarketTrades) < 15 {
+		w.Container.Render(screen)
+		return
+	}
+
+	for i, mt := range MarketTrades {
+		w.rows[14-i].price.Label = fmt.Sprintf("%.2f", mt.Price)
+		w.rows[14-i].quantity.Label = fmt.Sprintf("%.5f", mt.Quantity)
+		w.rows[14-i].time.Label = mt.Time.Format(time.TimeOnly)
+
+		if i > 0 && mt.Price >= MarketTrades[i-1].Price {
+			w.rows[14-i].price.Color = settings.Green
+			w.rows[14-i].quantity.Color = settings.Green
+		} else {
+			w.rows[14-i].price.Color = settings.Red
+			w.rows[14-i].quantity.Color = settings.Red
+		}
+	}
+
+	if lastPrice != MarketTrades[14].Price {
+		rect := w.rows[0].GetWidget().Rect
+
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(float64(rect.Dx()), float64(rect.Dy()))
+		op.GeoM.Translate(float64(rect.Min.X), float64(rect.Min.Y))
+		op.Blend = ebiten.BlendSourceOver
+		screen.DrawImage(w.flash, op)
+	}
+
+	lastPrice = MarketTrades[14].Price
+	w.Container.Render(screen)
 }
